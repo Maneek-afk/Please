d548febc71a08133751d61c30023f3601c83fd65
Derin9 <derinkuas@hotmail.com>
2025-11-18 19:02:54 +0000

    FINAL

--- Assets/Scenes/Scripts/SoundManager.cs
+++ Assets/Scenes/Scripts/SoundManager.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 
+
 public class SoundManager : MonoBehaviour
 {
     public AudioSource backgroundMusic;
     public AudioSource sfxSource;
 
     public AudioClip fartsfx;
+    public AudioClip eatsfx;
+    public AudioClip backgroundMusicClip;
+    public AudioClip endSfx;
+
+    void Start()
+    {
+        backgroundMusic.clip = backgroundMusicClip;
+        backgroundMusic.loop = true;
+        backgroundMusic.Play();
+    }
 
     public void PlayFartSound()
     {
         sfxSource.PlayOneShot(fartsfx);
     }
 
+    public void PlayEatSound()
+    {
+        sfxSource.PlayOneShot(eatsfx);
+    }
+
+    public void PlayEndSound()
+    {
+        sfxSource.PlayOneShot(endSfx);
+    }
 }
